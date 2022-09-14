04fc44de6d93ba8cce06c4a14308c15d55a8e0d5
Aransiola Daniel <adebayoddaniel@gmail.com>
2022-09-14 01:39:41 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,1 @@
 #include "main.h"
-/**
- * main - print putchar 
- * Return: always 0
- */
-
-int main(void)
-{
-	char *sh = "Putchar";
-
-	while (*sh)
-	{
-		_putchar(*sh)
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
-}
