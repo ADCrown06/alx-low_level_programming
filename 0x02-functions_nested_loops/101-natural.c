4aa31849b82b03495f47c140cf672d1ac98e99b1
Aransiola Daniel <adebayoddaniel@gmail.com>
2022-09-14 12:15:03 -0700

    excess line fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Lists all the natural numbers below 1021 that are multiples of 3 or 5.
+ * main - Lists all the natural numbers below 1021 multiples of 3 or 5.
  *
  * Return: Always 0.
  */
