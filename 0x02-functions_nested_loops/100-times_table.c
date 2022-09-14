37bff3e33d95b7c6e457872f9fc3dda4e29aff06
Aransiola Daniel <adebayoddaniel@gmail.com>
2022-09-14 11:59:50 -0700

    Changes made

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 
 
-	int num, mult, prod; 
+	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -17,7 +17,7 @@ void print_times_table(int n)
 		{
 			_putchar('0');
 
-			for (mult = 1; mult <=n; mult++)
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -29,12 +29,12 @@ void print_times_table(int n)
 				if (prod <= 9)
 					_putchar(' ');
 
-				if (prod >=100)
+				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (prod <=99 && prod >= 10)
+				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
 				}
