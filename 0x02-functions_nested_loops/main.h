441928f6b94b24058b3c5261c1e8b61c892f7ab2
Aransiola Daniel <adebayoddaniel@gmail.com>
2022-09-14 04:05:27 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef
-HOLBERTON_H_INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
@@ -11,7 +10,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(int a, int b);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
