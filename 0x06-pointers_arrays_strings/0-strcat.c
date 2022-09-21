4a1364a7a650810fe77ed622cd6d7111f878f834
Aransiola Daniel <adebayoddaniel@gmail.com>
2022-09-21 13:49:18 -0700

    daniel is just good

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
+ * strcat - Concatenates the string pointed to by
  *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
